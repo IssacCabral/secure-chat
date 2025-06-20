f89c111af224969f46cb23b6ea334b5b1b6d5e0c
Clidenor Issac de Almeida Cabral <clidenorissac@gmail.com>
2025-06-20 20:41:04 -0300

    refactor: streamline handleServerAckConnection function and remove debug logs

--- src/handlers/client/handleServerAckConnection.ts
+++ src/handlers/client/handleServerAckConnection.ts
@@ -6,40 +6,34 @@ import { createDiffieHellman } from "@crypto/dh";
 import { ECDSA_sign } from "@crypto/ecdsa";
 import { Message, MessageType } from "@utils/message";
 
-const dhClient = createDiffieHellman(
-  DhParams.PRIME_LENGTH_IN_BITS,
-  DhParams.GENERATOR
-);
-
-const publicKeyDH = dhClient.getPublicKey().toString("base64");
-const privateKeyEcdsa = fs.readFileSync(
-  `${__dirname}/keys/client-private.pem`,
-  "utf-8"
-);
-
-// 1. Cliente assina sua chave pública DH
-const messageToSign = publicKeyDH + UserName.CLIENT;
-const sig_A = ECDSA_sign(messageToSign, privateKeyEcdsa);
-
-console.log({ sig_A });
-
-const package_to_send = {
-  publicKeyDH: publicKeyDH, // A
-  sign: sig_A, // sig_A
-  user: UserName.CLIENT, // username_cliente
-};
-
-console.log({ package_to_send });
-
 export function handleServerAckConnection(
-  serverMessage: Message,
+  _serverMessage: Message,
   socket: net.Socket
 ) {
-  console.log("Mensagem recebida no cliente", serverMessage);
+  const dhClient = createDiffieHellman(
+    DhParams.PRIME_LENGTH_IN_BITS,
+    DhParams.GENERATOR
+  );
+  const publicKeyDH = dhClient.getPublicKey().toString("base64");
+
+  const privateKeyEcdsa = fs.readFileSync(
+    `${__dirname}/../../keys/client-private.pem`,
+    "utf-8"
+  );
+
+  const messageToSign = publicKeyDH + UserName.CLIENT;
+  const sig_A = ECDSA_sign(messageToSign, privateKeyEcdsa);
+
+  const content = {
+    publicKeyDH: publicKeyDH, // A
+    sign: sig_A, // sig_A
+    user: UserName.CLIENT, // username_cliente
+  };
 
   const message: Message = {
     type: MessageType.CLIENT_SENDS_DH_KEY_AND_SIGNATURE,
-    content: "enviando chave assinada do client",
+    content,
   };
+
   socket.write(JSON.stringify(message));
 }
