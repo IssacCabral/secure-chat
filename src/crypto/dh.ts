f9db9cbcbaf0d5b6725e59ac9cd8f814e0178ce6
Clidenor Issac de Almeida Cabral <clidenorissac@gmail.com>
2025-06-20 23:26:30 -0300

    refactor: remove comments

--- src/crypto/dh.ts
+++ src/crypto/dh.ts
@@ -8,21 +8,3 @@ export function createDiffieHellman(
   dh.generateKeys();
   return dh;
 }
-
-// // Generate Alice's Key
-// const alice = createDiffieHellman(
-//   PublicParams.PRIME_LENGTH_IN_BITS,
-//   PublicParams.GENERATOR
-// );
-// const aliceKey = alice.generateKeys();
-
-// // Generate Bob's keys...
-// const bob = createDiffieHellman(alice.getPrime(), alice.getGenerator());
-// const bobKey = bob.generateKeys();
-
-// // Exchange and generate the secret...
-// const aliceSecret = alice.computeSecret(bobKey);
-// const bobSecret = bob.computeSecret(aliceKey);
-
-// console.log({ aliceSecret: aliceSecret.toString("hex") });
-// console.log({ bobSecret: bobSecret.toString("hex") });
